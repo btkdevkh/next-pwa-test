94578a19b7be4bd29993c36e7d69d66284833daf
Bunthoeun Kong <amcjkbtk91@outlook.com>
2025-02-26 01:22:29 +0100

    feat: :sparkles: Implement tickcounts

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
+import NivoResponsiveLine from "@/components/examples/nivo_example/NivoResponsiveLine";
+
 // Url "/"
 // This is a server component,
-
-import IndicatorList from "@/components/examples/react_select_example/IndicatorList";
-
 // it render a client/server component,
 const HomePage = () => {
   return (
     <>
-      <div className="mx-auto text-black">
-        <h1 className="text-lg text-white">Testing Functionality</h1>
+      <div className="mx-auto text-white">
+        <h1 className="text-lg">Testing Functionality</h1>
         <hr />
         <br />
 
         {/* Testing component */}
-        <IndicatorList />
+        <NivoResponsiveLine />
       </div>
     </>
   );
